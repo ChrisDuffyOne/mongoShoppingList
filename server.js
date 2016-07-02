7be83f210d57973acec73f5a834412c65f913bb7
Chris Duffy <chrisduffyone@gmail.com>
2016-07-02 10:34:45 +0000

    Added Enviromental port option

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use('*', function(request, response){
     response.status(404).json({message: 'Not Found'});
 });
 
-app.listen(8080, function(){
+//app.listen(8080, function(){ //Can't seem to bind $Port trying this
+app.listen(process.env.PORT || 8080, function(){
    console.log('Listening on port 8080'); 
 });
 
